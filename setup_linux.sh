ab0d2decbd62fe8e8b0ba9be420d9a356df386df
Tung Do <tungdo21899@gmail.com>
2023-09-12 15:20:14 +0700

    feat: install vs-code & tool development

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,1 +1,14 @@
+#!/bin/sh
+# Update nothing
+sudo apt-get update
+sudo apt-get upgrade -y
 
+# Install vs-code
+sudo apt-get update
+sudo apt install software-properties-common apt-transport-https wget -y
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code -y
+
+# Install tool development
+sudo apt-get install git vim curl wget tmux -y
